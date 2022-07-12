cde18818d0810f9f7a60b91c3fb37a81c4771e30
ZKTanTerry <109129642+ZKTanTerry@users.noreply.github.com>
2022-07-12 15:05:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express"); //using the express web framework
 
 var movieController = require('./controllers/movieController'); // set movieController to the movieController class
 var app = express(); // set variable app to be an instance of express framework. From now on, app is the express
-
+const port  = 3000
 app.use(express.static("./public")); //static files are to be served from the public folder - for eg. html, images, css
 app.use(express.json()); // json() is a method inbuilt in express to recognize the incoming Request Object from the web client as a JSON Object.
 // In time to come we will need to accept new or edited comments from user
@@ -11,5 +11,5 @@ app.route('/movies').get(movieController.getAllMovies); // activate the getAllMo
 
 
 
-app.listen(8080, "127.0.0.1"); // start the nodejs to be listening for incoming request @ port 8080
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 console.log("web server running @ http://127.0.0.1:8080"); // output to console 
